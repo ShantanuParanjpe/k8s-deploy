3b45f89607db91a453c9e65b12b00c63f9eab4b4
Your Name <you@example.com>
2023-03-14 18:58:16 +0000

    changed tf files

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "instance-1" {
   }
 }
 
-resource "aws_instance" ""instance-2 {
+resource "aws_instance" "instance-2" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
