cbbf1bf6391e0bff527d682d9599e4c45bceb12c
Your Name <you@example.com>
2023-03-14 18:53:48 +0000

    changed tf files

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example-"
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "instance-1" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id              = aws_subnet.example.id
+
+  tags = {
+    Name = "instance-1"
+  }
+}
+
+resource "aws_instance" ""instance-2 {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id              = aws_subnet.example.id
+
+  tags = {
+    Name = "instance-2"
+  }
+}
+
+
