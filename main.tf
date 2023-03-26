e327c3bebc15f9cc028390298cb846f7d0572d1d
Shantanu Paranjpe <shantanu.paranjpe@intive.com>
2023-03-26 13:45:02 +0100

    changes

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port = 22
     to_port   = 22
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
