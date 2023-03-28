6c5c08fb706a145c47a8b7d54aea59ea58b4ca93
Shantanu Paranjpe <shantanu.paranjpe@intive.com>
2023-03-28 12:53:02 +0100

    changes

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "example" {
 }
 
 resource "aws_instance" "instance-1" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-067a8829f9ae24c1c"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.example.id]
   subnet_id              = aws_subnet.example-subnet.id
@@ -44,7 +44,7 @@ resource "aws_instance" "instance-1" {
 }
 
 resource "aws_instance" "instance-2" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-067a8829f9ae24c1c"
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.example.id]
